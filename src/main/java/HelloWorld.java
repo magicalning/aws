9d5772b891427719554f7a35bfb8bf724fdb3436
magicalning <magicalning@126.com>
2022-07-05 10:05:38 +0800

    local modify bd

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world local modify g");
         System.out.println("hello world local modify b and remote modify g");
-        System.out.println("hello myBranch");
+        System.out.println("hello myBranchbb");
 
     }
 }
